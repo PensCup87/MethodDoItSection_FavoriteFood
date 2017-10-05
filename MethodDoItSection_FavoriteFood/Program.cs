aee1049da5dc0e2e5256c1e783935584c9cfd569
PensCup87 <bdt9123@gmail.com>
2017-10-04 20:25:32 -0400

    Examples Complete, Have to get clairification on WageCalculator

--- MethodDoItSection_FavoriteFood/Program.cs
+++ MethodDoItSection_FavoriteFood/Program.cs
@@ -10,6 +10,28 @@ namespace MethodDoItSection_FavoriteFood
     {
         static void Main(string[] args)
         {
+            FavoriteFood("Uncle Artie", "sauage and peppers");
+
+            RetirementCalculator(36);
+
+            WageCalculator(40.0, 24.79);
+        }
+        public static void FavoriteFood(string chef, string food)
+        {
+            Console.WriteLine(chef + "'s favorite dish is " + food);
+        }
+    public static void RetirementCalculator(int UserAge)
+        {
+            int retirementAge = 65 - UserAge;
+            Console.WriteLine("The user will retire in " + retirementAge + " years.");
+            return;
+        }
+
+    public double WageCalculator(double HoursWorked, double HourlyWage)
+        {
+            double monthlyWage = HoursWorked * HourlyWage;
+            Console.WriteLine("The monthly wages are $" + monthlyWage);
+            return;
         }
     }
 }
